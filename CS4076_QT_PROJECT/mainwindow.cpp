0e9d3f6b76f9c993b45284483ff14ed6f5a22139
Stephen <93441585+Spuddytime@users.noreply.github.com>
2023-03-23 14:45:53 +0000

    Basic QMenu added

--- CS4076_QT_PROJECT/mainwindow.cpp
+++ CS4076_QT_PROJECT/mainwindow.cpp
@@ -6,10 +6,33 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Create a menu bar
+    QMenuBar *menuBar = new QMenuBar(this);
+    setMenuBar(menuBar);
+
+    // Create a "File" menu
+    QMenu *fileMenu = menuBar->addMenu(tr("File"));
+
+    // Add "New" action
+    QAction *newAction = new QAction(tr("New"), this);
+    fileMenu->addAction(newAction);
+
+    // Add "Open" action
+    QAction *openAction = new QAction(tr("Open"), this);
+    fileMenu->addAction(openAction);
+
+    // Add "Exit" action
+    QAction *exitAction = new QAction(tr("Exit"), this);
+    connect(exitAction, &QAction::triggered, this, &QWidget::close); // Close the main window when the action is triggered
+    fileMenu->addAction(exitAction);
+
+    // Set the main window title and size
+    setWindowTitle(tr("Cooking Recipe Application"));
+    setFixedSize(800, 600);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
