8d0687a1325870ba6a49c8032a09ee6b20dd1b1f
Stephen <93441585+Spuddytime@users.noreply.github.com>
2023-03-23 15:40:30 +0000

    recipe window basic structure added

---
+++ CS4076_QT_PROJECT/recipeWindow.cpp
@@ -0,0 +1,22 @@
+#include <ui_mainwindow.h>
+
+RecipeWindow::RecipeWindow(QWidget *parent)
+    : QDialog(parent)
+{
+    // Create the widgets
+    nameEdit = new QLineEdit(this);
+    ingredientsEdit = new QTextEdit(this);
+    instructionsEdit = new QTextEdit(this);
+
+    // Set up the layout
+    QGridLayout *layout = new QGridLayout(this);
+    layout->addWidget(new QLabel(tr("Name:"), this), 0, 0);
+    layout->addWidget(nameEdit, 0, 1);
+    layout->addWidget(new QLabel(tr("Ingredients:"), this), 1, 0);
+    layout->addWidget(ingredientsEdit, 1, 1);
+    layout->addWidget(new QLabel(tr("Instructions:"), this), 2, 0);
+    layout->addWidget(instructionsEdit, 2, 1);
+    layout->addWidget(new QPushButton(tr("OK"), this), 3, 0, 1, 2);
+
+    setWindowTitle(tr("New Recipe"));
+}
