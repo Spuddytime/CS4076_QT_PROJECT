8d9f51ba6f423c7ee32c7aefc96ea4f393609cbd
Stephen <93441585+Spuddytime@users.noreply.github.com>
2023-03-23 15:41:08 +0000

    Added recipe window requirements to header file and to cmake

--- CS4076_QT_PROJECT/mainwindow.h
+++ CS4076_QT_PROJECT/mainwindow.h
@@ -3,7 +3,11 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "qtextedit.h"
 #include <QMainWindow>
+#include <QDialog>
+#include <QTextEdit>
+#include <QLineEdit>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -22,5 +26,18 @@ public slots:
 
 private:
     Ui::MainWindow *ui;
+};
+
+class RecipeWindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    RecipeWindow(QWidget *parent = nullptr);
+
+private:
+    QLineEdit *nameEdit;
+    QTextEdit *ingredientsEdit;
+    QTextEdit *instructionsEdit;
 };
 #endif // MAINWINDOW_H
