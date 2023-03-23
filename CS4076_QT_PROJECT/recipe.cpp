79f350ffbadf1485c4cb9e102e06ba569d8dd49b
Stephen <93441585+Spuddytime@users.noreply.github.com>
2023-03-23 19:01:45 +0000

    Added recipe.cpp

---
+++ CS4076_QT_PROJECT/recipe.cpp
@@ -0,0 +1,33 @@
+//Name:Stephen Walsh ID:21334234
+
+#include "recipe.h"
+#include "qdebug.h"
+
+Recipe::Recipe(QString name, QString ingredients, QString instructions)
+    : m_name(name), m_ingredients(ingredients), m_instructions(instructions)
+{
+}
+
+void Recipe::printRecipe()
+{
+    qDebug() << "Recipe: " << m_name;
+    qDebug() << "Ingredients: " << m_ingredients;
+    qDebug() << "Instructions: " << m_instructions;
+}
+
+void Recipe::setName(const QString& name)
+{
+    m_name = name;
+}
+
+void Recipe::setIngredients(const QString& ingredients)
+{
+    m_ingredients = ingredients;
+}
+
+void Recipe::setInstructions(const QString& instructions)
+{
+    m_instructions = instructions;
+}
+
+void Recipe::addRecipe
