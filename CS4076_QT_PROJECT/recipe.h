b0c3ff49d4fc00da5678f82485271364493d1598
Stephen <93441585+Spuddytime@users.noreply.github.com>
2023-03-23 16:46:19 +0000

    Recipe.h updated

--- CS4076_QT_PROJECT/recipe.h
+++ CS4076_QT_PROJECT/recipe.h
@@ -1,4 +1,21 @@
 #ifndef RECIPE_H
 #define RECIPE_H
 
+#include <QString>
+
+class Recipe
+{
+public:
+    Recipe(const QString& name, const QString& ingredients, const QString& instructions);
+
+    QString getName() const;
+    QString getIngredients() const;
+    QString getInstructions() const;
+
+private:
+    QString name;
+    QString ingredients;
+    QString instructions;
+};
+
 #endif // RECIPE_H
