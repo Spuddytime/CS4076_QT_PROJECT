9e45f1b3138a05b5eeb2de7f0d424d9fe7ad1922
Stephen <93441585+Spuddytime@users.noreply.github.com>
2023-03-29 16:18:34 +0100

    Testing a whole new recipe class

--- CS4076_QT_PROJECT/recipebook.h
+++ CS4076_QT_PROJECT/recipebook.h
@@ -1,23 +1,30 @@
 //Name:Stephen Walsh ID:21334234
 
-#ifndef RECIPEBOOK_H
-#define RECIPEBOOK_H
-
 #include <vector>
 #include "recipe.h"
 
 class RecipeBook
 {
 public:
-    RecipeBook();
-    void addRecipe(Recipe recipe);
-    Recipe getRecipe(int index);
-    int getRecipeCount();
-
     void addRecipe(const Recipe& recipe);
+    Recipe& getRecipe(int index);
+    int getSize();
 
 private:
-    std::vector<Recipe> recipes;
+    std::vector<Recipe> m_recipes;
 };
 
-#endif // RECIPEBOOK_H
+void RecipeBook::addRecipe(const Recipe& recipe)
+{
+    m_recipes.push_back(recipe);
+}
+
+Recipe& RecipeBook::getRecipe(int index)
+{
+    return m_recipes[index];
+}
+
+int RecipeBook::getSize()
+{
+    return m_recipes.size();
+}
